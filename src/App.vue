09da84dc309c5c72ae881430f77a6e4a47844136
Andres Dorado <andres.dorado90@gmail.com>
2022-02-26 23:20:49 +0100

    First commit for a Kamban project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <HelloWorld />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'LayoutDefault',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    HelloWorld
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
+  }
 }
-</style>
+</script>
