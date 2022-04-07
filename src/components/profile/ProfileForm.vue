fecafb53a373041da408c2b3a15e69f3da9afa24
Andres Dorado <andres.dorado90@gmail.com>
2022-04-07 21:13:06 +0200

    User profile form in readonly mode implemented. Pending edit mode

--- src/components/profile/ProfileForm.vue
+++ src/components/profile/ProfileForm.vue
@@ -1,18 +1,90 @@
 <template>
   <q-card flat square>
-      <q-card-section>
-          
-      </q-card-section>
-
+    <q-card-section class="q-mb-md">
+      <h3 class="q-mb-none">
+        Profile
+        <q-btn
+          flat
+          color="primary"
+          size="1.2rem"
+          icon="create"
+          @click="edit = !edit"
+        >
+          <q-tooltip> Edit profile </q-tooltip>
+        </q-btn>
+      </h3>
+      <small class="text-grey">User ID: {{ userId }}</small>
+    </q-card-section>
+    <q-card-section>
+      <!-- Display name -->
+      <q-input
+        v-model="displayName"
+        label="User name"
+        name="display_name"
+        type="text"
+        aria-required="true"
+        lazy-rules
+        :readonly="!edit"
+        :hint="
+          edit
+            ? 'Type your user name. It will be displayed in the profile page.'
+            : null
+        "
+      >
+      </q-input>
+      <!-- Email -->
+      <q-input
+        v-model="email"
+        label="Email"
+        name="email"
+        type="email"
+        aria-required="true"
+        lazy-rules
+        :readonly="!edit"
+        :hint="
+          edit
+            ? 'Type your email. It will be used to login to the system.'
+            : null
+        "
+      >
+      </q-input>
+      <!-- Phone -->
+      <q-input
+        v-model="phone"
+        label="Phone Number"
+        name="phone"
+        type="phone"
+        aria-required="true"
+        lazy-rules
+        :readonly="!edit"
+        :hint="
+          edit
+            ? 'Ingress your phone number. '
+            : null
+        "
+      >
+      </q-input>
+    </q-card-section>
   </q-card>
 </template>
 
 <script>
-export default {
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
 
-}
+export default {
+  setup() {
+    const store = useStore();
+    const edit = ref(false);
+    const userId = computed(() => store.state.user.userId);
+    const displayName = computed(() => store.state.user.userInfo?.displayName);
+    const email = computed(() => store.state.user.userInfo?.email);
+    const phone = computed(
+      () => store.state.user.userInfo?.phoneNumber || "No phone"
+    );
+    return { userId, displayName, email, phone, edit };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,9 +5,9 @@
         <avatar-manager />
       </div>
       <div class="col-11 col-md-6">
-        <h2>Profile</h2>
-        <pre>{{ userInfo }}</pre>
-        <q-btn to="/settings" label="to Settings"></q-btn>
+        <profile-form />
+        <!-- <pre>{{ userInfo }}</pre> -->
+        <q-btn to="/settings" label="Settings"></q-btn>
       </div>
     </div>
   </q-page>
@@ -17,10 +17,11 @@
 import { useStore } from "vuex";
 import { computed } from "vue";
 import AvatarManager from "@/components/profile/AvatarManager.vue";
+import ProfileForm from "@/components/profile/ProfileForm.vue";
 
 export default {
   name: "Profile",
-  components: { AvatarManager },
+  components: { AvatarManager, ProfileForm },
   setup() {
     const store = useStore();
 
